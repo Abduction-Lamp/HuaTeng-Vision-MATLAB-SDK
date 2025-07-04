f28cefc31243595a36d76a58ab7525dcfc908ad5
Vladimir <vladimir.lesnykh@cosmos.ru>
2025-07-04 15:52:49 +0300

    testing automatic generator protofile

--- Protofile/generator.m
+++ Protofile/generator.m
@@ -9,3 +9,18 @@ if not(libisloaded(SDK))
 end
 
 disp(warnings)
+
+if not(libisloaded(SDK))
+    [notfound, warnings] = loadlibrary(path_dll_x64, @Proto_Camera_SDK_x64, 'alias', SDK);
+    % unloadlibrary(SDK);
+end
+disp(warnings)
+libfunctionsview(SDK)
+
+status = calllib(SDK, 'CameraSdkInit', 0)
+device_count = calllib(SDK, 'CameraEnumerateDeviceEx')
+
+device_info = libstruct('tSdkCameraDevInfo')
+device_info.uInstance = uint32(7)
+
+structs.tSdkCameraDevInfo.members=struct('acProductSeries', 'int8#32', 'acProductName', 'int8#32', 'acFriendlyName', 'int8#32', 'acLinkName', 'int8#32', 'acDriverVersion', 'int8#32', 'acSensorType', 'int8#32', 'acPortType', 'int8#32', 'acSn', 'int8#32', 'uInstance', 'uint32');
